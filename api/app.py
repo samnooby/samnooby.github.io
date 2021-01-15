4e506dd8920fd6b89d67f251edef7b36e0b551d7
sam <sam.newby@mail.utoronto.ca>
2021-01-15 04:35:13 -0500

    Adds production flags

--- api/app.py
+++ api/app.py
@@ -3,7 +3,7 @@ from flask_restful import Api
 from flask_cors import CORS
 from Running.MomRunning import RunnerRoute, RunRoute,\
     DeleteRunRoute
-
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -14,4 +14,5 @@ api.add_resource(RunRoute, "/runs/<string:runner_name>")
 api.add_resource(DeleteRunRoute, "/runs/<string:runner_name>/delete")
 
 if __name__ == "__main__":
-    app.run(port=8888)
+    server_port = os.environ.get("PORT", "8080")
+    app.run(debug=False, port=server_port, host='0.0.0.0')
