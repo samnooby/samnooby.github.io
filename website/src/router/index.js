0ca24d765d71dcc2bd8473ff28e44e49a4dcba36
Sam <sam.newby@mail.utoronto.ca>
2021-01-18 18:28:10 +0000

    Updates title and icon

--- website/src/router/index.js
+++ website/src/router/index.js
@@ -1,27 +1,40 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+// import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
+//   {
+//     path: "/",
+//     name: "Home",
+//     component: Home,
+//   },
+//   {
+//     path: "/about",
+//     name: "About",
+//     component: () =>
+//       import(/* webpackChunkName: "about" */ "../views/About.vue"),
+//   },
   {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
-  {
-    path: "/running",
+    path: "/steps",
     name: "Running",
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/Running.vue"),
+    meta: {
+        title: "Michelle Steps Challenge"
+    }
   },
+  {
+      path: "/",
+      name: "Pagenotfound",
+      component: {
+          template: <p>Page not found</p>
+      },
+      meta: {
+          title: 'Page Not Found'
+      }
+  }
 ];
 
 const router = new VueRouter({
@@ -29,5 +42,12 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes,
 });
+
+const DEFAULT_TITLE = 'SamNewby';
+router.afterEach((to) => {
+    Vue.nextTick(() => {
+        document.title = to.meta.title || DEFAULT_TITLE;
+    });
+})
 
 export default router;
