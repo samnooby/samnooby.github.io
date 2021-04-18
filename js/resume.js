23b2b66bddca77e252233c145c6defe2ee94d8b5
Sam Newby <sam.newby@mail.utoronto.ca>
2021-04-18 18:12:12 -0400

    Adds resume page (needs updating)

--- js/resume.js
+++ js/resume.js
@@ -1,1 +1,17 @@
-console.log("Loaded resume javascript")
+window.addEventListener('load', function() {
+    $(document).ready(function () {
+        var typing_animation = {
+            width: '10em'
+        }
+        var respone_animation = {
+            opacity: '1'
+        }
+    
+        $("#navigation-date").animate(respone_animation, 200)
+            $("#show-navigation").animate(typing_animation, 1700, () => {
+                $("#nav-menu").animate(respone_animation, 200)
+            })
+    
+        console.log("Loaded resume page javascript")
+    })
+    });    
