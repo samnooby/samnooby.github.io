22a7787501b87cec72c779f1013aeaec74368264
Sam Newby <sam.newby@mail.utoronto.ca>
2021-04-18 17:32:18 -0400

    Adds about page content

--- js/about.js
+++ js/about.js
@@ -1,1 +1,22 @@
-console.log("Loaded about javascript")
+window.addEventListener('load', function() {
+    $(document).ready(function () {
+        var typing_animation = {
+            width: '10em'
+        }
+        var respone_animation = {
+            opacity: '1'
+        }
+    
+        $("#about-command").addClass("typed")
+        $("#about-command").animate(typing_animation, 1700, () => {
+            $("#response").animate(respone_animation, 200)
+            $("#portrait").animate(respone_animation, 200)
+            $("#navigation-date").animate(respone_animation, 200)
+            $("#show-navigation").animate(typing_animation, 1700, () => {
+                $("#nav-menu").animate(respone_animation, 200)
+            })
+        })
+    
+        console.log("Loaded about page javascript")
+    })
+    });    
