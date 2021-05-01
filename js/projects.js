b31186ad4fdf61be54794af0293e8d5aa8e9e927
Sam Newby <sam.newby@mail.utoronto.ca>
2021-05-01 14:50:30 -0400

    Adds project page content

--- js/projects.js
+++ js/projects.js
@@ -1,1 +1,21 @@
-console.log("Loaded projects javascript")
+window.addEventListener('load', function() {
+    $(document).ready(function () {
+        var typing_animation = {
+            width: '10em'
+        }
+        var respone_animation = {
+            opacity: '1'
+        }
+    
+        $("#projects-command").addClass("typed")
+        $("#projects-command").animate(typing_animation, 1700, () => {
+            $("#project-container").animate(respone_animation, 200)
+            $("#navigation-date").animate(respone_animation, 200)
+            $("#show-navigation").animate(typing_animation, 1700, () => {
+                $("#nav-menu").animate(respone_animation, 200)
+            })
+        })
+    
+        console.log("Loaded project page javascript")
+    })
+    });    
